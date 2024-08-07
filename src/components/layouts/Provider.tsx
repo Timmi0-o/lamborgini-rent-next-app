70123fa58ec2e693f0bbb3f7d17f364729f0e6ca
Timmi0-o <tima53419@gmail.com>
2024-08-07 18:55:33 +0700

    updata: import header & footer layout

--- src/components/layouts/Provider.tsx
+++ src/components/layouts/Provider.tsx
@@ -1,32 +1,19 @@
 'use client'
-import Lenis from 'lenis'
-import { ReactNode, useEffect } from 'react'
+import { ReactLenis } from 'lenis/react'
+import { ReactNode } from 'react'
+import { Footer } from './Footer/Footer'
+import { Header } from './Header/Header'
 
 export const Provider = ({ children }: { children: ReactNode }) => {
-	useEffect(() => {
-		const lenis = new Lenis({
-			easing: function easeInOutBack(x) {
-				const c1 = 1.70158
-				const c2 = c1 * 1.525
-
-				return x < 0.5
-					? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-					: (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2
-			},
-			lerp: 0.05,
-		})
-
-		const raf = (time: number) => {
-			lenis.raf(time)
-			requestAnimationFrame(raf)
-		}
-
-		requestAnimationFrame(raf)
-
-		return () => {
-			lenis.destroy()
-		}
-	}, [])
-
-	return <div>{children}</div>
+	return (
+		<div>
+			<ReactLenis root options={{ lerp: 0.06 }}>
+				<div>
+					<Header />
+					{children}
+					<Footer />
+				</div>
+			</ReactLenis>
+		</div>
+	)
 }
